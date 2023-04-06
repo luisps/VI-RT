e8eef98bdf883351ea7e58f862cd4fdd989b7ae5
Luís Paulo Santos <psantos@di.uminho.pt>
2023-04-06 14:10:43 +0100

    Added support for light sources to identify their type

--- VI-RT/Light/AmbientLight.hpp
+++ VI-RT/Light/AmbientLight.hpp
@@ -13,10 +13,12 @@
 class AmbientLight: public Light {
 public:
     RGB color;
-    AmbientLight (RGB _color): color(_color) { isAmbient = true; }
+    AmbientLight (RGB _color): color(_color) { type = AMBIENT_LIGHT; }
     ~AmbientLight () {}
     // return the Light RGB radiance for a given point : p
     RGB L (Point p) {return color;}
+    // return the Light RGB radiance
+    RGB L () {return color;}
     // return a point p and RGB radiance for a given probability pair prob[2]
     RGB Sample_L (float *prob, Point *p) {
         p = NULL;
